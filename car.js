fc686839cd34f8455288505ba247bfaec8dcec38
tobiasnf <tobiasnf@gmail.com>
2021-02-18 12:39:54 +0100

    .

--- car.js
+++ car.js
@@ -1,7 +1,10 @@
 
 export default class Car {
     constructor({ canvasID, carColor }) {
-        this.ctx = document.getElementById(canvasID).getContext('2d')
+        this.canvas = document.getElementById(canvasID)
+        this.canvas.width = 500
+        this.canvas.height = 500
+        this.ctx = this.canvas.getContext('2d')
         this.coord = { x: 250, y: 250 }
         this.carColor = carColor
         this.height = 25
@@ -9,6 +12,8 @@ export default class Car {
         this.degrees = 0
         this.turnRate = 0
         this.isDriving = false
+        this.radius = Math.sqrt((this.height / 2) ** 2 + (this.width / 2) ** 2)
+        this.angel = (Math.acos((this.width / 2) / this.radius) * 2) / (Math.PI / 180)
     }
 
     getX = (degrees) => this.coord.x + (this.radius * Math.sin((Math.PI * 2) / 360 * degrees))
@@ -17,17 +22,22 @@ export default class Car {
     create() {
         this.degrees += this.turnRate
         this.radius = Math.sqrt((this.height / 2) ** 2 + (this.width / 2) ** 2)
-        let angel = (Math.acos((this.width / 2) / this.radius) * 2) / (Math.PI / 180)
-        let carShape = new Path2D()     
-        carShape.moveTo(this.getX(this.degrees - 90 - angel / 2), this.getY(this.degrees - 90 - angel / 2))
-        carShape.lineTo(this.getX(this.degrees - 90 + angel / 2), this.getY(this.degrees - 90 + angel / 2))
-        carShape.lineTo(this.getX(this.degrees + 90 - angel / 2), this.getY(this.degrees + 90 - angel / 2))
-        carShape.lineTo(this.getX(this.degrees + 90 + angel / 2), this.getY(this.degrees + 90 + angel / 2))
-
-        //this.ctx.arc(this.coord.x, this.coord.y, this.radius, 0, 2 * Math.PI)
+        let carShape = new Path2D()
+        carShape.moveTo(this.getX(this.degrees - 90 - this.angel / 2), this.getY(this.degrees - 90 - this.angel / 2))
+        carShape.lineTo(this.getX(this.degrees - 90 + this.angel / 2), this.getY(this.degrees - 90 + this.angel / 2))
+        carShape.lineTo(this.getX(this.degrees + 90 - this.angel / 2), this.getY(this.degrees + 90 - this.angel / 2))
+        carShape.lineTo(this.getX(this.degrees + 90 + this.angel / 2), this.getY(this.degrees + 90 + this.angel / 2))
 
         this.ctx.fillStyle = this.carColor
         this.ctx.fill(carShape)
+
+        //this.ctx.arc(this.coord.x, this.coord.y, this.radius, 0, 2 * Math.PI)
+
+        let square = new Path2D()
+        square.rect(300, 200, 100, 100)
+        this.ctx.fillStyle = 'pink'
+        this.ctx.fill(square)
+
         this.ctx.stroke()
     }
     remove() {
@@ -41,33 +51,44 @@ export default class Car {
         this.height -= removeRange
         this.width -= removeRange
     }
+    getColorInFront() {
+        console.log(this.ctx.getImageData(
+            this.getX(this.degrees - 90 - this.angel / 2),
+            this.getY(this.degrees - 90 - this.angel / 2),
+            1, 1).data
+        )
+        console.log(this.ctx.getImageData(
+            this.getX(this.degrees - 90 + this.angel / 2),
+            this.getY(this.degrees - 90 + this.angel / 2),
+            1, 1).data
+        )
+    }
     turn(amount) {
         this.turnRate = amount
     }
     move(amount) {
         this.remove()
-        let xAmount = (Math.cos(2*Math.PI*(this.degrees/360))) * amount
-        let yAmount = (Math.sin(2*Math.PI*(this.degrees/360))) * amount
+        let xAmount = (Math.cos(2 * Math.PI * (this.degrees / 360))) * amount
+        let yAmount = (Math.sin(2 * Math.PI * (this.degrees / 360))) * amount
 
         this.coord.x += xAmount
         this.coord.y += yAmount
         this.create()
     }
     animate() {
-        if (this.isDriving) {
-            this.move(3)
-            this.stopID = window.requestAnimationFrame(() => this.animate())
-        }
+        this.move(0.3)
+        this.stopID = window.requestAnimationFrame(() => this.animate())
     }
     drive() {
         this.isDriving = !this.isDriving
-        this.animate()
+        if (this.isDriving) {
+            this.animate()
+        } else {
+            window.cancelAnimationFrame(this.stopID)
+        }
     }
 
     test() {
-        this.remove()
-        this.degrees += 10
-        this.create()
-
+        this.getColorInFront()
     }
 }
