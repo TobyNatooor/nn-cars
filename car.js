6adaee7ec72c9cae6f40d18ef9744cbb29e2b891
tobiasnf <tobiasnf@gmail.com>
2021-02-18 12:06:49 +0100

    .

--- car.js
+++ car.js
@@ -1,31 +1,28 @@
 
 export default class Car {
-    constructor(canvasID, carColor) {
+    constructor({ canvasID, carColor }) {
         this.ctx = document.getElementById(canvasID).getContext('2d')
         this.coord = { x: 250, y: 250 }
         this.carColor = carColor
-        this.height = 80
-        this.width = 150
-        this.degrees = 62
+        this.height = 25
+        this.width = 40
+        this.degrees = 0
         this.turnRate = 0
         this.isDriving = false
-        this.radius = Math.sqrt((this.height / 2) ** 2 + (this.width / 2) ** 2)
     }
 
     getX = (degrees) => this.coord.x + (this.radius * Math.sin((Math.PI * 2) / 360 * degrees))
     getY = (degrees) => this.coord.y - (this.radius * Math.cos((Math.PI * 2) / 360 * degrees))
 
     create() {
-        this.degrees = (this.degrees % 360) + this.turnRate
-        if (this.degrees < 0) this.degrees = 359.9
-
+        this.degrees += this.turnRate
         this.radius = Math.sqrt((this.height / 2) ** 2 + (this.width / 2) ** 2)
-        let angel = (Math.acos(((this.width / 2)) / this.radius) * 2) / (Math.PI / 180)
-        let carShape = new Path2D()
-        carShape.moveTo(this.getX(this.degrees + 0), this.getY(this.degrees + 0))
-        carShape.lineTo(this.getX(this.degrees + angel), this.getY(this.degrees + angel))
-        carShape.lineTo(this.getX(this.degrees + 180), this.getY(this.degrees + 180))
-        carShape.lineTo(this.getX(this.degrees + 180 + angel), this.getY(this.degrees + 180 + angel))
+        let angel = (Math.acos((this.width / 2) / this.radius) * 2) / (Math.PI / 180)
+        let carShape = new Path2D()     
+        carShape.moveTo(this.getX(this.degrees - 90 - angel / 2), this.getY(this.degrees - 90 - angel / 2))
+        carShape.lineTo(this.getX(this.degrees - 90 + angel / 2), this.getY(this.degrees - 90 + angel / 2))
+        carShape.lineTo(this.getX(this.degrees + 90 - angel / 2), this.getY(this.degrees + 90 - angel / 2))
+        carShape.lineTo(this.getX(this.degrees + 90 + angel / 2), this.getY(this.degrees + 90 + angel / 2))
 
         //this.ctx.arc(this.coord.x, this.coord.y, this.radius, 0, 2 * Math.PI)
 
@@ -49,31 +46,16 @@ export default class Car {
     }
     move(amount) {
         this.remove()
-        let xAmount
-        if (62 > this.degrees) {
-            xAmount = ((2 / -180) * (62 - this.degrees) + 1) * amount
-            console.log((62 - this.degrees).toFixed(2), this.degrees.toFixed())
-        }
-        else if (62 <= this.degrees && this.degrees < 242) {
-            xAmount = ((2 / -180) * (this.degrees - 62) + 1) * amount
-            console.log((this.degrees - 62).toFixed(2), this.degrees.toFixed())
-        }
-        else if (this.degrees >= 242) {
-            xAmount = ((2 / -180) * ((this.degrees - 62)-(((this.degrees - 62) % 180))) + 1) * amount
-            console.log(((this.degrees - 62)-((this.degrees - 62) % 180)).toFixed(2), this.degrees.toFixed())
-        }
+        let xAmount = (Math.cos(2*Math.PI*(this.degrees/360))) * amount
+        let yAmount = (Math.sin(2*Math.PI*(this.degrees/360))) * amount
 
-        // console.log(`xAmount:${xAmount.toFixed(2)}`)
-        // console.log(`Degrees:${this.degrees.toFixed(2)}`)
-
-        // let yAmount
         this.coord.x += xAmount
-        // this.coord.y += yAmount
+        this.coord.y += yAmount
         this.create()
     }
     animate() {
         if (this.isDriving) {
-            this.move(1)
+            this.move(3)
             this.stopID = window.requestAnimationFrame(() => this.animate())
         }
     }
--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
 import Car from './car.js'
 
-let c = new Car('theCanvas', 'green')
+let c = new Car({
+    canvasID: 'theCanvas',
+    carColor: 'green'
+})
 
 c.create()
 
 document.getElementById('driveBtn').addEventListener('click', () => c.drive())
 
 document.getElementById('leftBtn')
-    .addEventListener('click', () => { c.turn(-0.4) })
+    .addEventListener('click', () => { c.turn(-1) })
 
 document.getElementById('forwardBtn')
     .addEventListener('click', () => { c.turn(0) })
 
 document.getElementById('rightBtn')
-    .addEventListener('click', () => { c.turn(0.4) })
+    .addEventListener('click', () => { c.turn(1) })
 
 document.getElementById('testBtn').addEventListener('click', () => {
     c.test()
